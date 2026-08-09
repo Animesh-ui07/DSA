88c8a3b79e3d495caf9b8a29cc9c78956e9eadb0
Animesh-ui7 <mishra.animesh2k07@gmail.com>
2026-08-10 03:48:38 +0530

    linear search

---
+++ 01_Array/linear_search.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std ;
+
+int linearSearch(int arr[] , int sz , int target) {
+    for (int i = 0 ; i<sz ; i++) {
+        if (arr [i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main () {
+    int arr[] = {4,5,6,7,8} ;
+    int sz = 5;
+    int target = 7;
+
+    cout << linearSearch(arr , sz , target) << endl;
+    return 0;
+}
+
