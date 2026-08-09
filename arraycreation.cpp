02ee57eb49e9fc53d1042b5a79c97902ef8b4221
Animesh-ui7 <mishra.animesh2k07@gmail.com>
2026-08-10 02:14:11 +0530

    Array code 1

---
+++ arraycreation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void arraycreation () {
+    int marks[100];
+}
+
+void arrayinsertion () {
+    int marks[4] = {2,3,4,5};
+    int rollno[4]= {21,22,23,24};
+}
+
+int main () {
+    arraycreation();
+    arrayinsertion();
+    return 0;
+}
