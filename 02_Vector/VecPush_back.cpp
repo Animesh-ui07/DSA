5df9899d39658caa01ad999f68e4dcc1be79d335
Animesh-ui7 <mishra.animesh2k07@gmail.com>
2026-08-14 21:54:12 +0530

    vector function pushback

---
+++ 02_Vector/VecPush_back.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std ;
+
+int main () {
+    vector<int>vec;
+
+    cout << "size = " << vec.size() << endl;
+    vec.push_back(25);
+    cout << "after push back size = " << vec.size() << endl;
+
+    for (int val : vec) {
+        cout << val << endl;
+    }
+    return 0;
+}
